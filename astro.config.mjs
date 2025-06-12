7754616ccd78613b40d83d7f921a8a1398aa3504
ullasbc02 <ullasbc02@gmail.com>
2025-06-12 19:36:06 -0400

    update gh page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
+import github from "@astrojs/github";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: "https://ullasbc02.github.io/ullas-basavapatna-chandrashekar-online",
+  output: "static",
+  integrations: [tailwind(), github()],
 });
